508b1cc672f6d5c78a62495bd041673f564916fa
DimaShabatyn <dmitriyshabatyn@gmail.com>
2023-07-11 16:31:15 +0300

    refactor CRUD operations, implemented a new router to update the contact status

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,27 @@ const { Router } = require("express");
 
 const ctrl = require("../../controllers/contacts-controller");
 
-const schemas = require("../../schemas/contacts-schemas");
+const { schemas } = require("../../models/contact");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
 const router = Router();
 
 router.get("/", ctrl.getAllContacts);
 
-router.get("/:id", ctrl.getContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
 
 router.post("/", validateBody(schemas.contactAddSchema), ctrl.addContact);
 
-router.delete("/:id", ctrl.removeContact);
+router.delete("/:id", isValidId, ctrl.removeContact);
 
-router.put("/:id", validateBody(schemas.contactAddSchema), ctrl.updateContact);
+router.put("/:id", isValidId, validateBody(schemas.contactAddSchema), ctrl.updateContact);
+
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
