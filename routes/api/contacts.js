45e55256a9732e22230998e90e59ff90fecbf61b
DimaShabatyn <dmitriyshabatyn@gmail.com>
2023-07-19 17:15:14 +0300

    add middleware auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,21 +5,30 @@ const ctrl = require("../../controllers/contacts-controller");
 const { schemas } = require("../../models/contact");
 
 const { validateBody, isValidId } = require("../../middlewares");
+const { authenticate } = require("../../middlewares");
 
 const router = Router();
+// router.use(authenticate);
 
-router.get("/", ctrl.getAllContacts);
+router.get("/", authenticate, ctrl.getAllContacts);
 
-router.get("/:id", isValidId, ctrl.getContactById);
+router.get("/:id", authenticate, isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schemas.contactAddSchema), ctrl.addContact);
+router.post("/", authenticate, validateBody(schemas.contactAddSchema), ctrl.addContact);
 
-router.delete("/:id", isValidId, ctrl.removeContact);
+router.delete("/:id", authenticate, isValidId, ctrl.removeContact);
 
-router.put("/:id", isValidId, validateBody(schemas.contactAddSchema), ctrl.updateContact);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.contactAddSchema),
+  ctrl.updateContact
+);
 
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
